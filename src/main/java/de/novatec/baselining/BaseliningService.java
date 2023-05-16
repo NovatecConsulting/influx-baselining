f0ed0e1a093850756c2eac82002fe95ccd98f8ac
Jochen Just <jochen.just@novatec-gmbh.de>
2023-05-16 13:09:19 +0200

    Uses Spring Boot 3.0.x

--- src/main/java/de/novatec/baselining/BaseliningService.java
+++ src/main/java/de/novatec/baselining/BaseliningService.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
--- src/main/java/de/novatec/baselining/config/measurement/MeasurementFieldName.java
+++ src/main/java/de/novatec/baselining/config/measurement/MeasurementFieldName.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 @Data
 @NoArgsConstructor
