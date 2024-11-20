9ad6e5b4df963ebd368c114706f295b9bedfbbfd
EddeCCC <eduard.schander@gmx.de>
2024-11-20 16:25:24 +0100

    test

--- src/main/java/de/novatec/baselining/Application.java
+++ src/main/java/de/novatec/baselining/Application.java
@@ -1,11 +1,9 @@
 package de.novatec.baselining;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class Application {
 
     public static void main(String[] args) {
