d81dabc0191bfd0f74ff39a2defd1e391c1b106c
EddeCCC <eduard.schander@gmx.de>
2024-11-20 16:17:16 +0100

    update dependencies

---
+++ src/test/java/de/novatec/baselining/ApplicationTest.java
@@ -0,0 +1,13 @@
+package de.novatec.baselining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Spring context loads");
+    }
+}
